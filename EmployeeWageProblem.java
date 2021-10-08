e0c200c552ed6ef3ae80c7a612c351ab2d68aa90
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-08 11:27:41 +0530

    Welcome to Employee Wage Computation Problem

---
+++ EmployeeWageProblem.java
@@ -0,0 +1,6 @@
+package com.company;
+public class EmployeeWageProblem {
+    public static void main (String[] args) {
+        System.out.println("Welcome To Employee Wage Compuation Program");
+    }
+}
