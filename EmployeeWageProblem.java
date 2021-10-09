440ba34e96603573442d6610e25b36a68840de50
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-09 12:35:55 +0530

    Employee Wage for multiple Company

---
+++ EmployeeWageProblem.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class EmployeeWageProblem {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program using OOP");
+
+        //creating object
+        EmployeeWageChildClass employeeData = new EmployeeWageChildClass();
+
+        //Getting Wages For Working Hour And Days for company
+        employeeData.wagesForWorkingHourAndDays("D-Mart",20,10,50);
+        employeeData.wagesForWorkingHourAndDays("Star Home",10,15,80);
+    }
+}
